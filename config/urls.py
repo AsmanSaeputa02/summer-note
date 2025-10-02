ddfdd4e734e3cbfad867289615784958b2aec19d
Asman <asman_s>
2025-10-02 17:09:12 +0700

    ฟ

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
 ]
 
-# --- 3. เพิ่มเงื่อนไขนี้เข้าไปที่ท้ายไฟล์ ---
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
