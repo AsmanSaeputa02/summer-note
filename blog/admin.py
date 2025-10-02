5d46269898f909b7eeee86aa67bf485b0d533d0d
Asman <asman_s>
2025-10-02 17:05:31 +0700

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+# blog/admin.py
+from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+admin.site.register(Post, PostAdmin)
